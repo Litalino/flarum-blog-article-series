a1cba437042534ec11218c94e19a34baff3d3101
Litalino <99712477+Litalino@users.noreply.github.com>
2023-12-12 16:28:59 +0700

    1.2.0

--- migrations/2021_06_29_000000_tags_is_article_series_column.php
+++ migrations/2021_06_29_000000_tags_is_article_series_column.php
@@ -1,5 +1,26 @@
 <?php
 
-return Flarum\Database\Migration::addColumns('tags', [
-    'is_article_series' => ['boolean', 'default' => false],
-]);
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Schema;
+
+return [
+    'up' => function (Builder $schema) {
+        if ($schema->hasTable('tags')) {
+            if (Schema::hasColumn('tags', 'is_article_series')) {
+                return;
+            }
+            return Flarum\Database\Migration::addColumns('tags', [
+                'is_article_series' => ['boolean', 'default' => false],
+            ]);
+        }
+    },
+    'down' => function (Builder $schema) {
+
+    },
+];
+
+//return Flarum\Database\Migration::addColumns('tags', [
+//    'is_article_series' => ['boolean', 'default' => false],
+//]);
--- migrations/2021_06_29_000001_discussions_article_series_columns.php
+++ migrations/2021_06_29_000001_discussions_article_series_columns.php
@@ -2,16 +2,26 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Schema;
 
 
 return [
     'up' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) {
-                $table->integer('article_series_id')->nullable()->unsigned();
-                $table->integer('article_series_order')->nullable();
+        if ($schema->hasTable('discussions')) {
+            if (Schema::hasColumn('discussions', 'article_series_id')) {
+                return;
+            }
 
-                $table->foreign('article_series_id')->references('id')->on('tags')->onDelete('set null');
-            });
+            if (Schema::hasColumn('discussions', 'article_series_order')) {
+                return;
+            }
+        }
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->integer('article_series_id')->nullable()->unsigned();
+            $table->integer('article_series_order')->nullable();
+
+            $table->foreign('article_series_id')->references('id')->on('tags')->onDelete('set null');
+        });
     },
     'down' => function (Builder $schema) {
         $schema->table('discussions', function (Blueprint $table) {
